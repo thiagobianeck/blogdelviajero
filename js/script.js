1c467b5aa88bbbec5b1a89a1eabc0ae8645d763f
Thiago Bianeck <thiagobianeck@gmail.com>
2019-06-18 00:15:32 -0300

    25. Aplicando efectos Parallax al Banner

--- js/script.js
+++ js/script.js
@@ -10,4 +10,21 @@ $('.fade-slider').jdSlider({
     interval: 5000,
     isCursor: false,
     speed: 3000
-});    
+});
+
+var alturaBanner = $('.fade-slider').height();
+$('.bannerEstatico').css({'height':`${alturaBanner}px`})
+
+$(window).scroll(function() {
+    var posY = window.pageYOffset;
+
+    if(posY > alturaBanner) {
+        $('header').css({'background': 'white'});
+        $('header .logotipo').css({'filter':'invert(100)'});
+        $('.fa-search, .fa-bars').css({'color':'black'});
+    } else {
+        $('header').css({'background': 'rgba(0,0,0,.5)'});
+        $('header .logotipo').css({'filter':'invert(0)'});
+        $('.fa-search, .fa-bars').css({'color':'white'});
+    }
+});
